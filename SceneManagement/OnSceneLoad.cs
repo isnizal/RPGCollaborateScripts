8622d00f0bf60260d71b9fb184569c1f4d668501
isnizal <31788317+isnizal@users.noreply.github.com>
2021-02-25 12:59:09 +0800

    Update OnSceneLoad.cs
    loading from start and load work.

--- SceneManagement/OnSceneLoad.cs
+++ SceneManagement/OnSceneLoad.cs
@@ -6,48 +6,53 @@ using UnityEngine.SceneManagement;
 public class OnSceneLoad : MonoBehaviour
 {
 	private Player thePlayer;
+	[SerializeField]
 	private GameObject createCharacterSelection;
+	private bool characterSelect;
 	private void Start()
 	{
-		//no character selection
-		if (FindObjectOfType<CharacterSelection>() == null)
+		findCharacterSelection();
+    }
+	void findCharacterSelection()
+	{
+		bool character = FindObjectOfType<CharacterSelection>();
+		if (!character)
 		{
-			//create character selection with warrior
-			ProtectedSaveFiles.Basic.SaveController.Load();
-			string currentCharacterSelection = ProtectedSaveFiles.Basic.SaveController.Data.SaveCharacterSelection;
-			var characSelect = Instantiate(createCharacterSelection);
-			characSelect.GetComponent<CharacterSelection>().ConfirmWarrior(currentCharacterSelection);
-			thePlayer = characSelect.GetComponent<CharacterSelection>().GetCharacterObject().GetComponent<Player>();
-			if (thePlayer != null)
-			{
-				//load the player profile
-				thePlayer.playerCurrentHP = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerHP;
-				thePlayer.playerCurrentMana = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerMana;
-				thePlayer.currentGold = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerGold;
-				thePlayer.playerExperience = ProtectedSaveFiles.Basic.SaveController.Data.SaveCurrentEXP;
-				thePlayer.playerPositionX = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerLocationX;
-				thePlayer.playerPositionY = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerLocationY;
+             //create character selection with warrior
+             ProtectedSaveFiles.Basic.SaveController.Load();
+             string currentCharacterSelection = ProtectedSaveFiles.Basic.SaveController.Data.SaveCharacterSelection;
+             var characSelect = Instantiate(createCharacterSelection);
+             characSelect.GetComponent<CharacterSelection>().ConfirmWarrior(currentCharacterSelection);
+             thePlayer = characSelect.GetComponent<CharacterSelection>().GetCharacterObject().GetComponent<Player>();
+             thePlayer.gameObject.SetActive(true);
+             if (thePlayer != null)
+             {
+                 //load the player profile
+                 thePlayer.playerCurrentHP = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerHP;
+                 thePlayer.playerCurrentMana = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerMana;
+                 thePlayer.currentGold = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerGold;
+                 thePlayer.playerExperience = ProtectedSaveFiles.Basic.SaveController.Data.SaveCurrentEXP;
+                 thePlayer.playerPositionX = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerLocationX;
+                 thePlayer.playerPositionY = ProtectedSaveFiles.Basic.SaveController.Data.SavePlayerLocationY;
 
-				thePlayer.transform.position = new Vector2(thePlayer.playerPositionX, thePlayer.playerPositionY);
-				Debug.Log("successfully load the profile");
-			}
-		}
-		//have character selection
-		else if(FindObjectOfType<CharacterSelection>() != null)
+                 thePlayer.transform.position = new Vector2(thePlayer.playerPositionX, thePlayer.playerPositionY);
+                 return;
+             }
+        }
+		else
 		{
-			thePlayer = FindObjectOfType<CharacterSelection>().GetCharacterObject().GetComponent<Player>();
-			if (!thePlayer.gameObject.activeInHierarchy)
-			{
-				thePlayer.gameObject.SetActive(true);
-				thePlayer = FindObjectOfType<Player>();
-				thePlayer.transform.position = GameObject.Find("FirstSpawn").transform.position;
-			}
-			else
-			{
-				SceneManager.sceneLoaded += CheckLastSceneLoad;
-			}
+           thePlayer = FindObjectOfType<CharacterSelection>().GetCharacterObject().GetComponent<Player>();
+           if (!thePlayer.gameObject.activeInHierarchy)
+           {
+               thePlayer.gameObject.SetActive(true);
+               thePlayer = FindObjectOfType<Player>();
+               thePlayer.transform.position = GameObject.Find("FirstSpawn").transform.position;
+           }
+           else
+           {
+               SceneManager.sceneLoaded += CheckLastSceneLoad;
+           }
 		}
-
 	}
     private void Update()
     {
